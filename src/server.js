be6ff4b2958d1642ffd7ff4ea5f8593730c69259
Mrk197 <mirkaalamilla97@gmail.com>
2023-11-23 20:33:36 -0600

    config deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express"); //Framework
 const morgan = require("morgan");
-const cors = require("cors"); // cors - políticas para controlar que y quien puede hacer peticiones al servidor 
+//const cors = require("cors"); // cors - políticas para controlar que y quien puede hacer peticiones al servidor 
 const router = require("./routes");
 
 const server = express();
@@ -8,7 +8,15 @@ const server = express();
 //Midlewares
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors());
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Router principal
 server.use(router);
